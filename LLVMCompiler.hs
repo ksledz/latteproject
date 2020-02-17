31755c3f5c0c9cde244458375c5846908048d4cf
ksledz <kamila.ktos.sledz@gmail.com>
2020-02-17 01:55:37 +0100

    coraz mniej todo

--- LLVMCompiler.hs
+++ LLVMCompiler.hs
@@ -1,4 +1,3 @@
--- XXXXXDDDDD
 
 module LLVMCompiler (allToLLVM) where
 
@@ -29,6 +28,7 @@ data LLVMRep =
   | LLCall Loc LLType Ident [(LLType, Loc)]
   | LLInternalConst Register Int String
   | LLSub Loc Loc Loc
+  | LLSubI1 Loc Loc Loc
   | LLAdd Loc Loc Loc
   | LLMul Loc Loc Loc
   | LLDiv Loc Loc Loc
@@ -41,6 +41,8 @@ data LLVMRep =
   | LLNE Loc Loc Loc
   | LLAnd Loc Loc Loc
   | LLOr Loc Loc Loc
+  | LLAloca LLType Loc
+  | LLStore LLType Loc Loc
 data LLType =
   I8 | I32 | I1 | Void | LLStar LLType
 type LLVMonad a =
@@ -67,13 +69,61 @@ generateCode :: [LLVMRep] -> String
 programToLLVM :: Program -> String
 programToLLVM (Program l) = generateCode (runLLVMonad (mapM_ topDefToLLVM l) )
 
+swapArgs :: Arg -> (Ident, Type)
+swapArgs (Arg t i) = (i, t)
+getIdentLoc :: Arg -> LLVMonad (Ident, Loc)
+getIdentLoc (Arg t i) = do
+  reg <- freshRegister
+  tell [LLAlloca (typeToLLType t), (LocAddress reg)]
+  return (i, LocAddress reg)
+
 topDefToLLVM :: TopDef -> LLVMonad ()
 topDefToLLVM (FnDef t ident args block) = do
-  censor ((:[]) . LLFunc (typeToLLType t) ident (map argToLLArg args)) $ blockToLLVM block
--- TODO #3 widoczność zmiennych
+  let tempList2 = map swapArgs args
+  tempList1 <- mapM getIdentLoc args
+  let tempMap1 = fromList tempList1
+      tempMap2 = fromList tempList2
+  local (\x -> (Map.union tempMap1 (fst x), Map.union tempMap2 (snd x))) $
+    censor ((:[]) . LLFunc (typeToLLType t) ident (map argToLLArg args)) $ blockToLLVM block
+
+getArgsList1 :: Type -> [Item] -> LLVMonad([(Ident, Loc)])
+getArgsList1 t [] = []
+getArgsList1 t (NoInit i:tail) = do
+  reg <- freshRegister
+  tell [LLAlloca (typeToLLType t), (LocAddress reg)]
+  generateDefaultConstructor (typeToLLType t) (LocAddress reg)
+  list <- getArgsList1 t tail
+  return ((i, LocAddress reg):list)
+
+
+getArgsList1 t (Init i e:tail) = do
+  reg <- freshRegister
+  tell [LLAlloca (typeToLLType t), (LocAddress reg)]
+  (_, l) <- exprToLLVM e
+  assign l (LocAddress reg) t
+  list <- getArgsList1 t tail
+  return ((i, LocAddress reg):list)
+generateDefaultConstructor :: LLType -> Loc -> LLVMonad ()
+
+generateDefaultConstructor I32 loc = tell [LLStore I32 (LLConst 0) loc]
+generateDefaultConstructor I1 loc = tell [LLStore I1 (LLConst 0) loc]
+generateDefaultConstructor (LLStar I8) loc = do
+  reg <- freshRegister
+  tell [LLCall (LocAddress reg) (LLStar I8) (Ident "malloc") [(I32, LLConst 1), (I32, LLConst 1]]
+  assign loc (LocAddress reg) (LLStar I8)
+
+getArgsList2 :: Type -> [Item] -> [(Ident, Type)]
+getArgsList2 t [] = []
+getArgsList2 t (NoInit i:tail) = [(i, t):(getArgsList2 t tail)]
+getArgsList2 t (Init i _:tail) = [(i, t):(getArgsList2 t tail)]
 blockToLLVM :: Block -> LLVMonad ()
 blockToLLVM (Block (Decl t items):tail) = do
-  --TODO #3 generacja deklaracji, mapa nazw, update readera
+  let tempList2 = getArgsList2 t items
+  tempList1 <- getArgsList1 t items
+  let tempMap1 = fromList tempList1
+      tempMap2 = fromList tempList2
+  local (\x -> (Map.union tempMap1 (fst x), Map.union tempMap2 (snd x))) $ blockToLLVM block
+
 blockToLLVM (Block (h:t)) = do
   stmtToLLVM h
   blockToLLVM (Block t)
@@ -107,7 +157,6 @@ exprToLLVM (ELitInt i) = return (I32, LLConst i)
 exprToLLVM ELitTrue = return (I1, LLConst 1)
 exprToLLVM ELitFalse = return (I1, LLConst 0)
 exprToLLVM (EApp i exprs) = do
-
   m <- mapM exprToLLVM exprs
   reg <- freshRegister
   t <- getType i
@@ -120,12 +169,11 @@ exprToLLVM (EString s) = do
   tell [LLInternalConst reg i s]
   return (LLStar I8, LocAddress reg)
 
--- TODO #1 jest 3 w nocy kurwa
 
 exprToLLVM EArr t expr = do
   reg  <- freshRegister
   (t1, l) <- exprToLLVm expr
-  tell [LLCall (LocAddress reg) LLStar (typeToLLType t) (Ident "malloc") [(t1, l), (I32, LLConst (width t)]]
+  tell [LLCall (LocAddress reg) (LLStar (typeToLLType t)) (Ident "malloc") [(t1, l), (I32, LLConst (width t)]]
   return (LLStar (typeToLLType t), (LocAddress reg))
 
 exprToLLVM Neg expr = do
@@ -134,25 +182,31 @@ exprToLLVM Neg expr = do
   tell [LLSub (LocReg reg) (LLConst 0) l]
   return (I32, (LocReg reg))
 
+exprToLLVM Not expr = do
+  reg <- freshRegister
+  (t, l) <- exprToLLVM expr
+  tell [LLSubI1 (LocReg reg) (LLConst 1) l]
+  return (I1, (LocReg reg))
+
 exprToLLVM (EMul expr1 op expr2) = do
   reg <- freshRegister
   (t1, l1) <- exprToLLVM expr1
   (t2, l2) <- exprToLLVM expr2
-  tell [mulOpToFn(op (LocReg reg) l1 l2)]
+  tell [mulOpToFn op (LocReg reg) l1 l2]
   return (I32, (LocReg reg))
 
 exprToLLVM (EAdd expr1 op expr2) = do
   reg <- freshRegister
   (t1, l1) <- exprToLLVM expr1
   (t2, l2) <- exprToLLVM expr2
-  tell [addOpToFn(op (LocReg reg) l1 l2)]
+  tell [addOpToFn op (LocReg reg) l1 l2]
   return (I32, (LocReg reg))
 
 exprToLLVM (ERel expr1 op expr2) = do
   reg <- freshRegister
   (t1, l1) <- exprToLLVM expr1
   (t2, l2) <- exprToLLVM expr2
-  tell [relOpToFn(op (LocReg reg) l1 l2)]
+  tell [relOpToFn op (LocReg reg) l1 l2 ]
   return (I1, (LocReg reg))
 
 
@@ -185,8 +239,6 @@ opToFn GE = LLGE
 opToFn EQU = LLEQU
 opToFn NE = LLNE
 
-
-width :: LLType -> Int
 getLocType :: Ident -> LLVMonad(LLType, Loc)
 getLocType i = do
   (map1, map2) <- ask
@@ -203,9 +255,13 @@ freshRegister :: LLVMonad Register
 freshRegister = do
   oldRegister <- get
   put $ oldRegister + 1
-  return $ "%XD" ++ show (oldRegister + 1)
+  return $ "%" ++ show (oldRegister + 1)
 
 assign :: Loc -> Loc -> LLType -> LLVMonad ()
+assign = undefined -- TODO #koniec
 
-assign = undefined -- TODO #2
-
+width ::LLType -> Int
+width I8 = 1
+width I32 = 4
+width I1 = 1
+width LLStar t = 8 -- ?
